6d32aa4b339a97bcd8d1e4a96cee7cfb5a231a92
Simson L. Garfinkel <simsong@acm.org>
2018-07-22 22:10:55 -0400

    added a tiny test program to show how zipfiles can be made to work with S3

---
+++ ziptest.py
@@ -0,0 +1,45 @@
+import zipfile
+
+class FakeFile:
+    """Like a real file, but prints what's happening."""
+    def __init__(self,name):
+        self.name = name
+        self.fp  = open(name,"rb")
+
+    def __repr__(self):
+        return "FakeFile<name:{} fp:{}>".format(self.name,self.fp)
+
+    def read(self,len=-1):
+        print("read({})".format(len))
+        return self.fp.read(len)
+
+    def seek(self,offset,whence):
+        print("will seek {},{}".format(offset,whence))
+        try:
+            r = self.fp.seek(offset,whence)
+        except Exception as e:
+            print("Exception: ",e)
+        print("seek({},{})={}".format(offset,whence,r))
+        return r
+
+    def tell(self):
+        r = self.fp.tell()
+        print("tell()={}".format(r))
+        return r
+
+    def write(self):
+        raise RuntimeError("Write not supported")
+
+    def flush(self):
+        raise RuntimeError("Flush not supported")
+
+    def close(self):
+        print("closed")
+        return self.fp.close()
+
+
+if __name__=="__main__":
+    ff = FakeFile("/Users/simsong/Downloads/OperaSetup.zip")
+    zf = zipfile.ZipFile(ff, mode='r', allowZip64=True)
+    print("name list:",zf.namelist())
+    
