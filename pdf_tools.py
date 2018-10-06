5d38a82982bb3201c0d6aaffaf9d87630a42284e
Simson L. Garfinkel <simson.l.garfinkel@census.gov>
2018-10-06 18:57:23 -0400

    updated

--- pdf_tools.py
+++ pdf_tools.py
@@ -1,58 +1,134 @@
 #
-# pdf_tools
+# pdftools
 # Simson Garfinkel
 # US Census Bureau
 
 import os
 import os.path
+import sys
+import zipfile
+import subprocess
+import time
+
+DOC_MAGIC=b"\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1"
+DOCX_MAGIC=b"PK"
+SOFFICE_DARWIN='/Applications/LibreOffice.app/Contents/MacOS/soffice'
+
+
+def check_magic(filepath,magic):
+    head = open(filepath,"rb").read(len(magic))
+    if head!=magic:
+        print("{} != {}".format(head,magic))
+        return False
+    return True
+
+def valid_docx(fname):
+    if check_magic(fname,DOCX_MAGIC)==False:
+        return False
+    # Make sure it's a valid ZIP file
+    try:
+        with zipfile.ZipFile(fname,"r") as zf:
+            return True
+    except zipfile.BadZipFile as e:
+        return False
+
+def valid_convertable_document_file(fname):
+    if not os.path.exists(fname):
+        return False            # doesn't exist
+
+    (name,ext) = os.path.splitext(fname.lower())
+    if ext=='.doc':
+        if os.path.getsize(fname) % 512 !=0:
+            return False
+        return  check_magic(fname,DOC_MAGIC)
+    if ext=='.docx':
+        return valid_docx(fname)
+    if ext=='.rtf':
+        return True
+    return False
+
+
 
 # https://stackoverflow.com/questions/30481136/pywin32-save-docx-as-pdf
 # https://stackoverflow.com/questions/6011115/doc-to-pdf-using-python
-def convert_doc_to_pdf(infile):
-    """Convert a Word .docx to PDF and stores the original file in a directory called converted.
-    Throws an exception if it can't convert.
+def quit_word():
+    """Quit Microsoft Word"""
+    if sys.platform=='win32':
+        word = win32com.client.Dispatch("Word.Application")
+        word.Quit()
+
+def convert_document_to_pdf(infile):
+    """Convert a .doc, .docx, or .rtf file to PDF. Converted file has the
+    same pathname, except .docx and been changed to .pdf. Throws an
+    exception if it can't convert.
     """
+    import os.path
+    if not os.path.exists(infile):
+        raise RuntimeError("{} does not exist".format(infile))
 
-    import os
     outfile = os.path.splitext(infile)[0] + ".pdf"
-
     if os.path.exists(outfile):
-        if os.path.getmtime(infile) < os.path.getmtime(outfile):
-            print("NO CONVERT {}: PDF ALREADY EXISTS".format(infile))
+        # Calculate the age of the PDF. Because of Windows errors, sometimes the PDF will be a few seconds
+        # younger than the .docx. In those cases, just ignore it.
+        pdf_age = int(os.path.getmtime(outfile)-os.path.getmtime(infile))
+        print("{} already exists (age={}s)".format(outfile,pdf_age))
+        if pdf_age > -30:
             return
-
     
-    print("CONVERT {} to PDF".format(infile))
-    try:
-        import sys,os,win32com.client,pywintypes
-    except ModuleNotFoundError as e:
-        print(e)
-        print("Cannot convert {} --- please convert it manually".format(infile))
-        raise e
+    print("CONVERT {}".format(infile))
+    print("    --> {}".format(outfile))
+    import sys
+    if sys.platform=='win32':
+        try:
+            import sys,os,win32com.client,pywintypes
+        except ModuleNotFoundError as e:
+            print(e)
+            print("Cannot convert {} --- please convert it manually".format(infile))
+            raise e
 
-    wdFormatPDF = 17
-    in_file = os.path.abspath(infile)
-    out_file = os.path.abspath(outfile)
-    word = win32com.client.Dispatch("Word.Application")
-    try:
-        doc = word.Documents.Open(in_file)
-    except pywintypes.com_error as e:
-        print("")
-        print("**** CANNOT CONVERT: {} *****".format(infile))
-        print(str(e))
-        print("")
-        exit(1)
-        
-    doc.SaveAs(out_file, FileFormat=wdFormatPDF)
-    doc.Close()
-    word.Quit()
+        wdFormatPDF = 17
+        in_file = os.path.abspath(infile)
+        out_file = os.path.abspath(outfile)
+        word = win32com.client.Dispatch("Word.Application")
+        try:
+            doc = word.Documents.Open(in_file)
+            doc.SaveAs(out_file, FileFormat=wdFormatPDF)
+            doc.Close()
+            word.Quit()
+        except pywintypes.com_error as e:
+            print("")
+            print("**** CANNOT CONVERT: {} *****".format(infile))
+            print(str(e))
+            print("")
+            word.Quit()
+            return None
+        word.Quit()
+        return outfile
+    if sys.platform=='darwin':
+        if not os.path.exists(SOFFICE_DARWIN):
+            print("{} not found.".format(SOFFICE_DARWIN),file=sys.stderr)
+            raise RuntimeError("DOCX conversion on MacOS requires LibreOffice to be installed")
+        outdir = os.path.dirname(infile)
+        cmd = [SOFFICE_DARWIN,'--headless','--convert-to','pdf','--outdir',outdir,infile]
+        r = subprocess.call(cmd)
+        outfile = os.path.splitext(infile)[0] + ".pdf"
+        #
+        # Strangely, the file does not immediately appear. So wait until it does, and timeout after 5 seconds
+        for i in range(500):
+            if os.path.exists(outfile):
+                return outfile
+            time.sleep(.01)
+        raise RuntimeError("{}: {} not created".format(" ".join(cmd),outfile))
 
-    return outfile
+#
+# soffice --headless --convert-to pdf filename.doc
+# libreoffice --headless --convert-to pdf filename.doc
+    raise RuntimeError("Please manually convert {}".format(infile))
 
 
 if __name__=="__main__":
     #
-    # Command oine to test the conversion
+    # Command to test the conversion
     #
     import sys
     ofn = convert_doc_to_pdf(sys.argv[1])
