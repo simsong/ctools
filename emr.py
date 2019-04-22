f61185e33419ce7c9a117989dd69248657aad79c
Simson Garfinkel <simson.l.garfinkel@census.gov>
2019-04-22 18:20:25 -0400

    finally figured out proxy

--- emr.py
+++ emr.py
@@ -55,6 +55,20 @@ def show_credentials():
     subprocess.call(['aws','configure','list'])
 
 
+def set_proxy(http=False,https=False):
+    """Individually control the HTTP and HTTPS proxy. It turns out that Amazon appears to use HTTP for instance IAM authentication  but HTTPS to reach the endpoints."""
+    if http:
+        os.environ[HTTP_PROXY]  = os.environ[BCC_HTTP_PROXY]
+    else:
+        if HTTP_PROXY in os.environ:
+            del os.environ[HTTP_PROXY]
+
+    if https:
+        os.environ[HTTPS_PROXY]  = os.environ[BCC_HTTPS_PROXY]
+    else:
+        if HTTPS_PROXY in os.environ:
+            del os.environ[HTTPS_PROXY]
+
 def proxy_on():
     os.environ[HTTP_PROXY]  = os.environ[BCC_HTTP_PROXY]
     os.environ[HTTPS_PROXY] = os.environ[BCC_HTTPS_PROXY]
