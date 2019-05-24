1d33f12d293f3476bd7cc59d907f91d5f76d7d0d
simson <simson.l.garfinkel@census.gov>
2019-05-24 10:36:42 -0400

    fixed spacing

--- tydoc.py
+++ tydoc.py
@@ -452,7 +452,7 @@ class TyTag(xml.etree.ElementTree.Element):
     def add_tag(self, tag, attrib={}, position=-1, **kwargs):
         return self.add_tag_elems(tag, elems=[], attrib=attrib, position=position, **kwargs)
 
-    def add_tag_text(self, tag, text, attrib={}, position=-1, **kwargs):
+    def add_tag_text(self, tag, text='', attrib={}, position=-1, **kwargs):
         """Like add_tag_elems above, but just with the text for tag. Calls add_tag_elems"""
         return self.add_tag_elems(tag, [text], attrib=attrib, position=position, **kwargs)
         
