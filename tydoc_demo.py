2ebefb0a9bda6618241c11c7fa94cd170ecc5563
Simson L. Garfinkel <simsong@acm.org>
2019-05-30 10:24:21 -0400

    fixed placement of images

--- tydoc_demo.py
+++ tydoc_demo.py
@@ -3,14 +3,16 @@
 # Demonstrate tydoc working with maplotlib
 
 # Make sure we will use matplot lib headless:
-import matplotlib
-matplotlib.use("Agg")
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg 
+
 
 import numpy as np
 import matplotlib.pyplot as plt
 from tydoc import *
+import latex_tools
 
-if __name__=="__main__":
+def demo1():
     x1 = np.linspace(0.0, 5.0)
     x2 = np.linspace(0.0, 2.0)
 
@@ -31,12 +33,40 @@ if __name__=="__main__":
 
     doc = tydoc()
     doc.h1("Matplotlib demo")
-    doc.p("This demonstrates how you can add ",b("matplotlib"),
-          " to your documents")
-    doc.insert_matplotlib(plt,dpi=72,pad_inches=0.1)
+    doc.p(["This demonstrates how you can add ",b("matplotlib"),
+           " to your documents"])
+    doc.append_matplotlib(plt,dpi=72,pad_inches=0.1)
     doc.p("Pretty neat, eh?")
-    doc.save("output.html",imagedir=".")
-    doc.save("output.tex")
-
-
+    doc.save("demo1.html")
+    doc.save("demo1.tex")
+    doc.save("demo1.md")
     
+    # Just for grins, let's also run LaTeX
+    latex_tools.run_latex("demo1.tex", delete_tempfiles=True)
+    
+def demo2():
+    doc = tydoc()
+    doc.h1("sin(x) graph")
+    doc.p("Here is a little graph of sin(x) from 0 to 4π")
+    doc.p("A second paragraph")
+    x = np.linspace(0, np.pi*4)
+    fig = Figure(figsize=(4,2))
+    FigureCanvasAgg(fig)
+    ax = fig.add_subplot(111)
+    ax.plot(x, np.sin(x))
+    doc.append_matplotlib(fig, dpi=72, pad_inches=0.1)
+    doc.p("A third paragraph")
+
+    doc.h1("sin(2x) graph")
+    doc.p("Here is a little graph of sin(2x) from 0 to π")
+    x = np.linspace(0, np.pi)
+    fig = Figure(figsize=(4,2))
+    FigureCanvasAgg(fig)
+    ax = fig.add_subplot(111)
+    ax.plot(x, np.sin(2*x))
+    doc.append_matplotlib(fig, dpi=72, pad_inches=0.1)
+    doc.save("demo2.html")
+
+if __name__=="__main__":
+    #demo1()
+    demo2()
