4fa8c6f4d5da194a144ab89d9fe2dc0108422776
Simson <simson.l.garfinkel@census.gov>
2019-05-31 18:08:22 -0400

    default timezone to NYC

--- dbfile.py
+++ dbfile.py
@@ -53,12 +53,65 @@ class DBSqlite3(DBSQL):
             print(f"Cannot open database file: {fname}")
             exit(1)
         
-class DBMySQL(DBSQL):
+
+class DBMySQLAuth:
     def __init__(self,*,host,database,user,password):
+        self.host = host
+        self.database = database
+        self.user=user
+        self.password = password
+
+
+class DBMySQL(DBSQL):
+    def __init__(self,auth):
         try:
             import mysql.connector as mysql
-            self.conn = mysql.connect(host=host,database=database,user=user,password=password)
+            self.conn = mysql.connect(host=auth.host,
+                                      database=auth.database,
+                                      user=auth.user,
+                                      password=auth.password)
+            self.dbs.cursor().execute('SET @@session.time_zone = "America/New_York"') # Census standard
+            self.dbs.cursor().execute('SET autocommit = 1') # autocommit
+
         except ImportError as e:
             print(f"Please install MySQL connector with 'conda install mysql-connector-python'")
             exit(1)
         
+    @staticmethod
+    RETRIES = 10
+    RETRY_DELAY_TIME = 1
+    def csfr(auth,cmd,vals=None,quiet=False):
+        """Connect, select, fetchall, and retry as necessary"""
+        import mysql.connector.errors
+        for i in range(1,self.RETRIES):
+            try:
+                db = DBMySQL(auth)
+                db.connect()
+                result = None
+                c = db.cursor()
+                try:
+                    logging.info(f"PID{os.getpid()}: {cmd} {vals}")
+                    if quiet==False:
+                        print(f"PID{os.getpid()}: cmd:{cmd} vals:{vals}")
+                    c.execute(cmd,vals)
+                except mysql.connector.errors.ProgrammingError as e:
+                    logging.error("cmd: "+str(cmd))
+                    logging.error("vals: "+str(vals))
+                    logging.error(str(e))
+                    raise e
+                if cmd.upper().startswith("SELECT"):
+                    result = c.fetchall()
+                c.close()       # close the cursor
+                db.close() # close the connection
+                return result
+            except mysql.connector.errors.InterfaceError as e:
+                logging.error(e)
+                logging.error(f"PID{os.getpid()}: NO RESULT SET??? RETRYING {i}/{self.RETRIES}: {cmd} {vals} ")
+                pass
+            except mysql.connector.errors.OperationalError as e:
+                logging.error(e)
+                logging.error(f"PID{os.getpid()}: OPERATIONAL ERROR??? RETRYING {i}/{self.RETRIES}: {cmd} {vals} ")
+                pass
+            time.sleep(self.RETRY_DELAY_TIME)
+        raise e
+
