5540247f2c93f1a40a9eff8f252cb2af5b2e0bc5
Pavel Zhuravlev <pavel.zhuravlev@census.gov>
2019-12-12 15:06:07 -0500

    Moved check

--- tydoc.py
+++ tydoc.py
@@ -509,8 +509,6 @@ class TyTag(xml.etree.ElementTree.Element):
         @param className - convenience method to specify attrib['class']
         Returns the tag that is added."""
 
-        if tag[0]=='<':
-            raise ValueError(f'tag {tag} should not include angle brackets.')
 
         if id is not None:
             if 'id' in attrib:
@@ -531,6 +529,8 @@ class TyTag(xml.etree.ElementTree.Element):
         if isinstance(tag, TyTag):
             e = tag
         else:
+            if tag[0] == '<':
+                raise ValueError(f'tag {tag} should not include angle brackets.')
             e = TyTag(tag, attrib=attrib)
         if position == -1:
             self.append(e)
