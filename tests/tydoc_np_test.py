cda829b7455e1ea41f6d93205117e75acd25b3cd
Simson Garfinkel <simsong@acm.org>
2020-06-14 20:59:52 -0400

    will not fail tests if numpy is not present

--- tests/tydoc_np_test.py
+++ tests/tydoc_np_test.py
@@ -3,13 +3,18 @@
 import sys
 import os
 import os.path
+import warnings
 
 sys.path.append( os.path.join( os.path.dirname(__file__), "../.."))
 
 from ctools.tydoc import tytable
 
 def test_tydoc_np():
-    import numpy
+    try:
+        import numpy
+    except ImportError:
+        warnings.warn("Cannot test numpy")
+        return
     data = numpy.array([1,2,3,4,5])
     doc = tytable()
     doc.add_head(["one",'two','three','four','five'])
