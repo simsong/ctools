3b70c323d367968ad85671ea54ad4ea061910850
Simson Garfinkel <simson.l.garfinkel@census.gov>
2020-10-28 13:26:38 -0400

    now throws warning rather than error

--- tests/clogging_test.py
+++ tests/clogging_test.py
@@ -30,7 +30,11 @@ def test_logging_to_syslog():
         warnings.warn(f"{LOCAL1_LOG} does not exist; cannot test logging to local1")
         return
 
-    clogging.setup(level=logging.INFO, syslog=True)
+    try:
+        clogging.setup(level=logging.INFO, syslog=True)
+    except ConnectionRefusedError as e:
+        warnings.warn('Cannot connect syslog server; we must be trying to tcp connect')
+        return
     nonce = str(time.time())
     logging.error("Logging at t={}.".format(nonce))
     # Wait a few milliseconds for the nonce to appear in the logfile
