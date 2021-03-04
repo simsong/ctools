a536fd1c09cc4425b2f6670f8c3e733147c93b24
Simson L. Garfinkel <simson.l.garfinkel@census.gov>
2021-03-04 10:20:22 -0500

    updated tests

--- env.py
+++ env.py
@@ -5,6 +5,7 @@ import os
 import re
 import pwd
 import sys
+import glob
 
 CENSUS_DAS_SH='/etc/profile.d/census_das.sh'
 VARS_RE   = re.compile(r"^(export)?\s*(?P<name>[a-zA-Z][a-zA-Z0-9_]*)=(?P<value>.*)$")
@@ -23,23 +24,38 @@ def get_vars(fname):
                     value = value[1:-1]
                 ret[name] = value
     return ret
-                
 
 
-def get_env(pathname):
+
+def get_env(pathname=None, *, profile_dir=None, prefix=None):
     """Read the BASH file and extract the variables. Currently this is
 done with pattern matching. Another way would be to run the BASH
 script as a subshell and then do a printenv and actually capture the
-variables"""
+variables
+    :param pathname: if provided, use this path
+    :param profile_dir: If provided, search this directory
+    :param prefix: if provided and profile_dir is provided, search for all files in the directory
+    :return: the variables that were learned.
+"""
+    if (pathname is not None) and (profile_dir is not None):
+        raise ValueError("pathname and profile_dir canot both be provided")
+    if (profile_dir is not None) and (prefix is None):
+        raise ValueError("If profile_dir is provided, pathname must be provided.")
+    if profile_dir:
+        names = sorted(glob.glob(os.path.join(profile_dir, prefix+"*")))
+        if len(names)==0:
+            raise FileNotFoundError(f"No file with prefix {prefix} in {profile_dir}")
+        pathname = names[0]
+
+    ret = {}
     for (key,val) in get_vars(pathname).items():
-        os.environ[key] = os.path.expandvars(val)
-        
-    
+        ret[key] = os.environ[key] = os.path.expandvars(val)
+    return ret
+
 
 def get_census_env():
-    """Read the file /etc/profile.d/census_das.sh and learn all of the
-environment variables"""
-    get_env(CENSUS_DAS_SH)
+    """Legacy to be deleted"""
+    return get_env(profile_dir = '/etc/profile.d', prefix = 'census')
 
 def get_home():
     """Return the current user's home directory without using the HOME variable. """
@@ -50,4 +66,3 @@ def dump(out):
     print("==== ENV ====",file=out)
     for (key,val) in os.environ.items():
         print(f"{key}={val}",file=out)
-    
--- tests/clogging_test.py
+++ tests/clogging_test.py
@@ -14,10 +14,7 @@ import time
 import platform
 import warnings
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-
-import ctools.clogging as clogging
+import clogging
 
 
 LOCAL1_LOG = '/var/log/local1.log'
@@ -49,11 +46,5 @@ def test_logging_to_syslog():
         warnings.warn("local1 is not logging to /var/log/local1.log")
 
     assert count in [0,1,2]
-    # Turned off shutting down logging because it breaks subsequent pytests that rely on TCP logging 
+    # Turned off shutting down logging because it breaks subsequent pytests that rely on TCP logging
     #clogging.shutdown()
-
-    
-
-
-
-    
