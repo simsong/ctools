17433cde274f636ae2b2fce8a71358f7b08dbb69
Simson L. Garfinkel <simson.l.garfinkel@census.gov>
2021-03-25 12:38:56 -0400

    uses boto3 for S3File

--- tests/s3_test.py
+++ tests/s3_test.py
@@ -7,17 +7,24 @@ import warnings
 import pytest
 import uuid
 import logging
+import zipfile
 
 from os.path import abspath
 from os.path import dirname
 
-sys.path.append(dirname(dirname(dirname(abspath(__file__)))))
+MY_DIR = dirname(abspath(__file__))
+TEST_FILE_DIR = os.path.join(MY_DIR,'test_files')
+PARENT_DIR = dirname(MY_DIR)
+
+if PARENT_DIR not in sys.path:
+    sys.path.append(PARENT_DIR)
 
 import ctools.s3 as s3
 
 MOTD = 'etc/motd'
 TEST_STRING  = "As it is written " + str(os.getpid()) + "\n"
 TEST_S3_FILE = "Examples/testfile.txt"
+TEST_ZIPFILE = os.path.join(TEST_FILE_DIR,'testfile.zip')
 
 def is_aws():
     return ("EC2_HOME" in os.environ or
@@ -71,7 +78,6 @@ def test_s3open(s3_tempfile):
     assert val1==val2==val3
 
 
-
 @pytest.mark.skipif(not is_aws(),reason='Not running on AWS')
 @pytest.mark.skipif(s3root() is None,reason='No S3ROOT defined')
 def test_s3open_write_fsync(s3_tempfile):
@@ -101,6 +107,17 @@ def test_s3open_iter(s3_tempfile):
     for i, l in enumerate(fl):
         assert l == TEST_STRING[:-1] + str(i) + "\n"
 
+
+@pytest.mark.skipif(not is_aws(),reason='Not running on AWS')
+@pytest.mark.skipif(s3root() is None,reason='No S3ROOT defined')
+def test_s3zipfile_support(s3_tempfile):
+    """See if we can read hello.txt from a zipfile we upload. This is a pretty good test."""
+    with s3.s3open(s3_tempfile, 'wb', fsync=True) as f:
+        f.write( open(TEST_ZIPFILE, 'rb').read() )
+    zf = zipfile.ZipFile( s3.S3File(s3_tempfile))
+    hf = zf.open('hello.txt')
+    hello = hf.read()
+    assert hello == b'Hello World!\n'
 
 if __name__=="__main__":
     test_s3open()
